932193ef4a282813bb61e706e9261449eed742ed
Alex Sung <alexssung@gmail.com>
2018-04-12 15:53:44 -0700

    Initial Keyword Scrape Form

--- app/javascript/components/KeywordScrapeForm.js
+++ app/javascript/components/KeywordScrapeForm.js
@@ -2,8 +2,25 @@ import React from 'react'
 
 export default class KeywordScrapeForm extends React.Component {
   render() {
+    const formStyle = {
+      margin: "20px"
+    }
+    
     return (
-      <div></div>
+      <form className="col-md-12" style={formStyle}>
+        <h4>New Keyword Scrape</h4>
+        <div className="form-group row">
+          <div className="col-md-6">
+            <label htmlFor="keywords">Keywords (comma separated)</label>
+            <textarea className="form-control" id="keywords" rows="1"></textarea>
+          </div>
+          <div className="col-md-6">
+            <label htmlFor="urls-csv">CSV of URLs (one per line)</label>
+            <input type="file" className="form-control-file" id="urls-csv"/>
+          </div>
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     )
   }
 }
